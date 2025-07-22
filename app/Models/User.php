ec6c930dea9f4e24e7029767737a3e33f276d3c9
Adamelbouchiri <matrakmtk@gmail.com>
2025-07-22 17:59:54 +0100

    adding-admin-seeder+roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
         'google_id',
         'facebook_id',
         'avatar',
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('role')->default('user');
             //OAuth Fields
             $table->string('google_id')->nullable();
             $table->string('facebook_id')->nullable(); 
